62fe0f611cd20aa6e2ba44e463a7d0bc8b1a729c
Damon Stamper <damon.stamper@gmail.com>
2018-04-23 23:29:58 -0400

    Putting ball back in middle of screen after last commit. AIPaddle code working and applied to right paddle. Adding .vs folder to gitignore.

--- Pong/Assets/Scripts/AIPaddle.cs
+++ Pong/Assets/Scripts/AIPaddle.cs
@@ -21,10 +21,36 @@ public class AIPaddle : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
+        // TODO: move Lerp in if/else to it's own function to DRY code.
+        // if the ball's y position is above the paddle:
         if (theBall.transform.position.y > this.transform.position.y)
         {
+            // Set paddle vector to go up
+            Vector2 dir = new Vector2(0, 1);
 
+            // per Lerp (?what is Lerp?) to smooth out movement
+            rigidBody.velocity = Vector2.Lerp(rigidBody.velocity,
+                dir * speed,
+                lerpTweak * Time.deltaTime);
         }
-		
-	}
+        // if the ball's y position is below the paddle:
+        else if (theBall.transform.position.y < this.transform.position.y)
+        {
+            // Set paddle vector to go down
+            Vector2 dir = new Vector2(0, -1);
+
+            // per Lerp (?what is Lerp?) to smooth out movement
+            rigidBody.velocity = Vector2.Lerp(rigidBody.velocity,
+                dir * speed,
+                lerpTweak * Time.deltaTime);
+        }
+        // if ball is neither up nor down relative to the paddle, then it must be vertically even so stop moving the paddle.
+        else {
+            Vector2 dir = new Vector2(0,0).normalized;
+            rigidBody.velocity = Vector2.Lerp(rigidBody.velocity,
+                dir * speed,
+                lerpTweak * Time.deltaTime);
+        }
+
+    }
 }
